f2634314f33411d732614ebb7fa588db87d58ed1
xoxoedg <rugyasin@gmail.com>
2021-12-13 21:10:48 +0100

    Initial Commit

---
+++ src/test/java/rug/coronaApi/CoronaTracking/CoronaTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package rug.coronaApi.CoronaTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
