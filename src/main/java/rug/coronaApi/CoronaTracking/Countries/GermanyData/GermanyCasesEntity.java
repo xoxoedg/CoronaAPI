99deb13b80afecd844d3824ba1317d0e2d64a4ee
xoxoedg <rugyasin@gmail.com>
2021-12-16 21:48:34 +0100

    Added Builder for Entity Class

--- src/main/java/rug/coronaApi/CoronaTracking/Countries/GermanyData/GermanyCasesEntity.java
+++ src/main/java/rug/coronaApi/CoronaTracking/Countries/GermanyData/GermanyCasesEntity.java
@@ -26,4 +26,52 @@ public class GermanyCasesEntity {
     private LocalDateTime createdDate;
     @UpdateTimestamp
     private LocalDateTime updatedDate;
+
+
+    public GermanyCasesEntity(String bundeslandName, BigInteger recovered, BigInteger deaths, BigInteger confirmed, LocalDateTime updated) {
+        this.bundeslandName = bundeslandName;
+        this.recovered = recovered;
+        this.deaths = deaths;
+        this.confirmed = confirmed;
+        this.updated = updated;
+    }
+
+    static class Builder {
+        private String bundeslandName;
+        private BigInteger recovered;
+        private BigInteger deaths;
+        private BigInteger confirmed;
+        private LocalDateTime casesUpdated;
+
+        public Builder bundeslandName(String name) {
+            this.bundeslandName = name;
+            return this;
+        }
+
+        public Builder recovered(BigInteger recovered) {
+            this.recovered = recovered;
+            return this;
+        }
+
+        public Builder deaths(BigInteger deaths) {
+            this.deaths = deaths;
+            return this;
+        }
+
+        public Builder confirmed(BigInteger confirmed) {
+            this.confirmed = confirmed;
+            return this;
+        }
+
+        public Builder casesUpdated(LocalDateTime time) {
+            this.casesUpdated = time;
+            return this;
+        }
+
+        public GermanyCasesEntity build() {
+            return new GermanyCasesEntity(bundeslandName, recovered, deaths, confirmed, casesUpdated);
+        }
+
+    }
+
 }
